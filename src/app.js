529f2524af1adf4adf864e716b7501cecd0b0d66
MNDZ19 <jorgemorenomendez19@gmail.com>
2025-03-24 12:24:40 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+  generarCarta();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById("CambiarCarta").addEventListener("click", generarCarta);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("cambiar-tamaño").addEventListener("click", cambiarTamaño)
 };
+
+function generarCarta() {
+  const palos = ["♥", "♠", "♣", "♦"];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+  document.getElementById("palo-top").innerHTML = paloAleatorio;
+  document.getElementById("valor").innerHTML = valorAleatorio;
+  document.getElementById("palo-bottom").innerHTML = paloAleatorio;
+
+  const carta = document.getElementById("carta");
+  carta.classList.remove("corazon", "diamante", "picas", "trebol");
+
+  if (paloAleatorio === "♥" || paloAleatorio === "♦") {
+    carta.classList.add("corazon");
+  } else {
+    carta.classList.add("picas");
+  }
+}
